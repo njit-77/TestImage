747eef83ef80f2d524b3c882638700ced8af1cc8
njit_77 <512424964@qq.com>
2020-05-23 14:34:31 +0800

    1、拖动增加图片边框限制;

--- TestImage/MainWindow.xaml.cs
+++ TestImage/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ namespace TestImage
         private const double ZoomOutScaleValue = 1.1;
         private const double ZoomInScaleValue = 1 / ZoomOutScaleValue;
         private Point start;
-        private Point origin;
 
         public MainWindow()
         {
@@ -27,8 +26,6 @@ namespace TestImage
             if (img != null)
             {
                 start = e.GetPosition(img);
-                origin.X = img.RenderTransform.Value.OffsetX;
-                origin.Y = img.RenderTransform.Value.OffsetY;
             }
         }
 
@@ -58,7 +55,6 @@ namespace TestImage
                 Matrix m = img.RenderTransform.Value;
 
                 SetImageBorder(ref m, p, img, control);
-
                 img.RenderTransform = new MatrixTransform(m);
             }
         }
@@ -83,15 +79,45 @@ namespace TestImage
 
         private void SetImageBorder(ref Matrix m, Point p, Image img, FrameworkElement control)
         {
-            double diffX = (p.X - start.X) * m.M11;
-            double diffY = (p.Y - start.Y) * m.M22;
+            double parentW = img.ActualWidth;                             /*容器控件宽*/
+            double parentH = img.ActualHeight;                            /*容器控件高*/
+            double xCount = parentW * m.M11;                              /*当前图像宽*/
+            double yCount = parentH * m.M22;                              /*当前图像高*/
+            double xLeft = (img.ActualWidth - control.ActualWidth) * 0.5; /*容器与图像宽的差*/
+            double xRight = control.ActualWidth - xCount + xLeft;         /*容器与图像宽的差*/
+            double yUp = (img.ActualHeight - control.ActualHeight) * 0.5; /*容器与图像高的差*/
+            double yBottom = control.ActualHeight - yCount + yUp;         /*容器与图像高的差*/
+            double diffX = (p.X - start.X);                               /*当前申请的平移X分量*/
+            double diffY = (p.Y - start.Y);                               /*当前申请的平移Y分量*/
             if (img.ActualWidth * m.M11 > control.ActualWidth)
             {
-                m.OffsetX = origin.X + diffX;
+                if (m.OffsetX + diffX < xRight)
+                {
+                    m.OffsetX = xRight;
+                }
+                else if (m.OffsetX + diffX > xLeft)
+                {
+                    m.OffsetX = xLeft;
+                }
+                else
+                {
+                    m.OffsetX += diffX;
+                }
             }
             if (img.ActualHeight * m.M22 > control.ActualHeight)
             {
-                m.OffsetY = origin.Y + diffY;
+                if (m.OffsetY + diffY < yBottom)
+                {
+                    m.OffsetY = yBottom;
+                }
+                else if (m.OffsetY + diffY > yUp)
+                {
+                    m.OffsetY = yUp;
+                }
+                else
+                {
+                    m.OffsetY += diffY;
+                }
             }
         }
 
